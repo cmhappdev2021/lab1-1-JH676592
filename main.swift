6747559a0d1f7efd66734a691b36288edf924233
JH676592 <jacksonholcomb01@gmail.com>
2020-08-31 18:31:59 +0000

    More print statements

--- main.swift
+++ main.swift
@@ -6,11 +6,11 @@ print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
 
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
-
+print("") // Return
 print("Bubbly by Colbie Caillat")
 
 // Use multiple print functions to write out some of the lyrics to the song.
-
+print("")  // Return
 print("It starts in my toes, make me crinkle my nose")
 print("Wherever it goes I always know")
 print("That you make me smile, please, stay for a while now")
