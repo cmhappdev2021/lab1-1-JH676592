abe051123e40cae2ca60ebdca24fb842e79cc4db
JH676592 <jacksonholcomb01@gmail.com>
2020-08-31 18:25:07 +0000

    Song print statements

--- main.swift
+++ main.swift
@@ -7,8 +7,11 @@ print("How to use playgrounds to make writing Swift fun and simple")
 
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
 
-
+print("Bubbly by Colbie Caillat")
 
 // Use multiple print functions to write out some of the lyrics to the song.
 
-
+print("It starts in my toes, make me crinkle my nose")
+print("Wherever it goes I always know")
+print("That you make me smile, please, stay for a while now")
+print("Just take your time wherever you go")
